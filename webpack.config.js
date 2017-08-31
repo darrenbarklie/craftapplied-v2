8033100858e06ece0d56d939db0efa59d9aa5255
Darren Barklie <darrenbarklie@gmail.com>
2017-08-31 13:06:31 +0100

    React installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/dist/',
+    //path: __dirname,
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
     loaders: [
       { // JSX Loader //
-        test: /\.js$/,
+        test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
